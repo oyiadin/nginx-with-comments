79b4acb261be0f214eeaafc16b1c871565df7033
Chen Xiaoyuan <oyiadin@users.noreply.github.com>
2022-11-06 21:14:36 +0800

    ngx_list注释

--- src/core/ngx_list.c
+++ src/core/ngx_list.c
@@ -9,6 +9,7 @@
 #include <ngx_core.h>
 
 
+// 创建新链表
 ngx_list_t *
 ngx_list_create(ngx_pool_t *pool, ngx_uint_t n, size_t size)
 {
@@ -19,6 +20,8 @@ ngx_list_create(ngx_pool_t *pool, ngx_uint_t n, size_t size)
         return NULL;
     }
 
+    // 创建第一个数组，大小 n * size
+    // 以及填入 ngx_list_t 的其他字段
     if (ngx_list_init(list, pool, n, size) != NGX_OK) {
         return NULL;
     }
@@ -27,6 +30,9 @@ ngx_list_create(ngx_pool_t *pool, ngx_uint_t n, size_t size)
 }
 
 
+// 在链表中追加一个新元素
+// 这里有点反直觉，新元素不是作为参数传进来的，而是作为返回值移交出去，需要由调用者自行填充新元素的取值
+// 给这个函数取个别名 ngx_alloc_in_list 可能更好理解
 void *
 ngx_list_push(ngx_list_t *l)
 {
@@ -36,22 +42,26 @@ ngx_list_push(ngx_list_t *l)
     last = l->last;
 
     if (last->nelts == l->nalloc) {
+        // 满了，建新数组
 
         /* the last part is full, allocate a new list part */
 
+        // 新数组的控制块（见 ngx_list_part_s）
         last = ngx_palloc(l->pool, sizeof(ngx_list_part_t));
         if (last == NULL) {
             return NULL;
         }
 
+        // 新数组，注意大小是 nalloc * size
         last->elts = ngx_palloc(l->pool, l->nalloc * l->size);
         if (last->elts == NULL) {
             return NULL;
         }
 
-        last->nelts = 0;
+        last->nelts = 0;  // 下边 L70 做了自增
         last->next = NULL;
 
+        // 把新数组的控制块链进链表
         l->last->next = last;
         l->last = last;
     }
@@ -61,3 +71,8 @@ ngx_list_push(ngx_list_t *l)
 
     return elt;
 }
+
+// 值得注意的是，没有 ngx_list_pop
+// OpenResty 好像也有类似设计，内存在同一请求内不会太主动释放
+// 等到请求结束才一起随着 ngx_pool_t 内存池一块释放
+// 所以对象池的概念在这里很有用：虽然请求结束后会释放，但请求结束之前不注意这些零碎内存的话，就会造成“临时的内存泄露”
--- src/core/ngx_list.h
+++ src/core/ngx_list.h
@@ -15,33 +15,38 @@
 
 typedef struct ngx_list_part_s  ngx_list_part_t;
 
+// 链表的组成部分，暂称为“链表数组控制块”
 struct ngx_list_part_s {
-    void             *elts;
-    ngx_uint_t        nelts;
-    ngx_list_part_t  *next;
+    void             *elts;     // 数组
+                                // 数组占用空间 = size * nalloc
+    ngx_uint_t        nelts;    // 已使用的元素数量，nelts <= nalloc
+    ngx_list_part_t  *next;     // 下一个数组
 };
 
 
+// ngx链表，链表的元素是数组，数组里才是最终要的元素
 typedef struct {
-    ngx_list_part_t  *last;
-    ngx_list_part_t   part;
-    size_t            size;
-    ngx_uint_t        nalloc;
-    ngx_pool_t       *pool;
+    ngx_list_part_t  *last;     // 链表尾部，方便 ngx_list_push 操作
+    ngx_list_part_t   part;     // 链表首个数组
+    size_t            size;     // 数组里元素的大小
+    ngx_uint_t        nalloc;   // 每个数组存储元素数量的上限
+    ngx_pool_t       *pool;     // 内存池对象
 } ngx_list_t;
 
 
 ngx_list_t *ngx_list_create(ngx_pool_t *pool, ngx_uint_t n, size_t size);
 
+// 初始化链表，ngx_list_create 会调用
 static ngx_inline ngx_int_t
 ngx_list_init(ngx_list_t *list, ngx_pool_t *pool, ngx_uint_t n, size_t size)
 {
+    // 每个数组大小为 n * size
     list->part.elts = ngx_palloc(pool, n * size);
     if (list->part.elts == NULL) {
         return NGX_ERROR;
     }
 
-    list->part.nelts = 0;
+    list->part.nelts = 0;  // 已使用（已拥有）的元素数量
     list->part.next = NULL;
     list->last = &list->part;
     list->size = size;
@@ -54,6 +59,7 @@ ngx_list_init(ngx_list_t *list, ngx_pool_t *pool, ngx_uint_t n, size_t size)
 
 /*
  *
+ *  ngx_list_t 典型的迭代过程
  *  the iteration through the list:
  *
  *  part = &list.part;
@@ -62,6 +68,7 @@ ngx_list_init(ngx_list_t *list, ngx_pool_t *pool, ngx_uint_t n, size_t size)
  *  for (i = 0 ;; i++) {
  *
  *      if (i >= part->nelts) {
+ *          // 如果当前数组迭代完了，切到下一个数组（如果还有的话）
  *          if (part->next == NULL) {
  *              break;
  *          }
